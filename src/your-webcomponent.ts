21ddfa281f017dba8925e6b2534ad91d1ed55718
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2021-06-02 12:11:08 -0700

    Initial commit

---
+++ src/your-webcomponent.ts
@@ -0,0 +1,28 @@
+import { html, css, LitElement, property, customElement } from 'lit-element';
+
+@customElement('your-webcomponent')
+export class YourWebComponent extends LitElement {
+  @property({ type: String }) title = 'Hey there';
+
+  @property({ type: Number }) counter = 5;
+
+  private increment() {
+    this.counter += 1;
+  }
+
+  render() {
+    return html`
+      <h2>${this.title}, Number: ${this.counter}!</h2>
+      <button @click=${this.increment}>increment</button>
+      <slot name="my-slot"> </slot>
+    `;
+  }
+
+  static styles = css`
+    :host {
+      display: block;
+      padding: 25px;
+      color: var(--your-webcomponent-text-color, #000);
+    }
+  `;
+}
