c5e69a44cd05190296f544ec7772ee7db5239526
Isa Herico-Velasco <isaherico@gmail.com>
2021-07-30 00:32:49 -0700

    start base view component

--- src/interfaces/nav-controller-interface.ts
+++ src/interfaces/nav-controller-interface.ts
@@ -1,6 +1,10 @@
 import { LitElement } from 'lit-element';
+import { IntMenuShortcut } from './menu-interfaces';
 
 export interface IntNavController extends LitElement {
   baseHost: string;
   itemMD: object;
+  menuProviders: object;
+  menuShortcuts: IntMenuShortcut[];
+  sideMenuOpen: boolean;
 }
