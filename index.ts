914bf6b1ba1dafeed40f8bcd08af0a71f8139527
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-08-31 16:04:49 -0700

    share menu main files + add to demo

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export { ItemNavigator } from './src/item-navigator';
+export { ItemNavigator } from './src/iaux-item-navigator';
 export { MenuButton } from './src/menu-slider/menu-button';
 export { IaMenuSlider } from './src/menu-slider/ia-menu-slider';
+
+// side menus
+export {
+  IauxSharingOptions,
+  iauxShareIcon,
+} from './src/menus/iaux-sharing-options';
+export { IauxViewableFiles } from './src/menus/iaux-viewable-files';
